ec655272ba44e7547ad771bc12427dd0c05bfe8f
申瑞珉 (Ruimin Shen) <45179423@qq.com>
2018-05-12 22:32:40 +0800

    fix

--- quick_start.sh
+++ quick_start.sh
@@ -1,3 +1,20 @@
+echo download COCO dataset
+LINKS="
+http://images.cocodataset.org/zips/train2014.zip
+http://images.cocodataset.org/zips/val2014.zip
+http://images.cocodataset.org/annotations/annotations_trainval2014.zip
+http://images.cocodataset.org/zips/train2017.zip
+http://images.cocodataset.org/zips/val2017.zip
+http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+"
+ROOT=~/data/coco
+for LINK in $LINKS
+do
+	aria2c --auto-file-renaming=false -d $ROOT $LINK
+	unzip -n $ROOT/$(basename $LINK) -d $ROOT
+done
+rm $ROOT/val2014/COCO_val2014_000000320612.jpg
+
 echo cache data
 python3 cache.py -c config.ini config/original_person18_19.ini -m cache/name=cache_original
 
